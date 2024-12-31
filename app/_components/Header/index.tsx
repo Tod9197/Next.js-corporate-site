8f1cc4cca20e2e27dc55dadeda50c8d45dfb80a9
Tod9197 <dancein18@yahoo.co.jp>
2024-12-31 11:22:45 +0900

    ハンバーガーメニューの実装

--- app/_components/Header/index.tsx
+++ app/_components/Header/index.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import styles from "./index.module.css";
 import Link from "next/link";
+import Menu from "../Menu";
 
 export default function Header() {
   return (
@@ -16,6 +17,7 @@ export default function Header() {
             priority
           />
         </Link>
+        <Menu />
       </header>
     </>
   );
