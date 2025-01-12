476da3074fbcde9f3afba597d142c9e44a0767e0
Tod9197 <dancein18@yahoo.co.jp>
2025-01-12 11:07:17 +0900

    サイトマップの追加

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,26 @@
 import "./globals.css";
+import type { Metadata } from "next";
 import Header from "./_components/Header";
 import Footer from "./_components/Footer";
+
+export const metadata: Metadata = {
+  metadataBase: new URL("https://next-js-corporate-site.vercel.app/"),
+  title: {
+    template: "%s | シンプルなコーポレートサイト",
+    default: "シンプルなコーポレートサイト",
+  },
+  description:
+    "「Next.js + ヘッドレスCMSではじめる！簡単・モダンWebサイト制作入門」で作成されるサイトです。",
+  openGraph: {
+    title: "シンプルなコーポレートサイト",
+    description:
+      "「Next.js + ヘッドレスCMSではじめる！簡単・モダンWebサイト制作入門」で作成されるサイトです。",
+    images: ["/ogp.png"],
+  },
+  alternates: {
+    canonical: "https://next-js-corporate-site.vercel.app/",
+  },
+};
 
 export default function RootLayout({
   children,
